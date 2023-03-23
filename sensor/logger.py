03de1352a9f405624bf9bfe8a89d0890f6dee318
JatinKumarMahaldar <jatinkumarmahaldar@gmail.com>
2023-03-23 22:19:07 +0530

    component,entities and logger file structure updated

---
+++ sensor/logger.py
@@ -0,0 +1,18 @@
+import logging
+from datetime import datetime
+import os
+
+
+LOG_FILE_NAME = f"{datetime().now().strftime('%m%d%Y_%H-%M-%S')}.log"
+LOG_FILE_DIRECTORY = os.path.join(os.getcwd(),"logs")
+
+#make directory if not present 
+os.makedirs(LOG_FILE_DIRECTORY,exist_ok=True)   
+
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIRECTORY,LOG_FILE_NAME)
+
+logging.basicConfig(
+    filename= LOG_FILE_PATH,
+    format= "[%(asctime)s] %(lineno)d | %(name)s | %(levelname)s | %(message)s",
+    level= logging.INFO
+)
